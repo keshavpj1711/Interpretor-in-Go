dfd26f63432e8094f247d765c9c947faafc2dbbc
keshavpj1711 <keshavpj1711@gmail.com>
2024-01-19 22:57:43 +0530

    short variable declaration added

---
+++ Starting_Steps_Basics/variables/short_variable_dec.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	// variables declaration
+	var phoneNum int = 10
+	fmt.Printf("%d\n",phoneNum)
+
+	// short variable declaration
+	phone := 102392
+	fmt.Printf("%d\n", phone)
+
+	fmt.Printf("Hello World")
+}
--- Starting_Steps_Basics/variables/variables_basic.go
+++ Starting_Steps_Basics/variables/variables_basic.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
+	// initialize variables here
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
 
+	// here if we declare variables it initializes itself to zero
 
-	fmt.Printf()
+	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
