7610cc1e91b55927910fdb873845055dacdde385
keshavpj1711 <keshavpj1711@gmail.com>
2024-01-20 19:10:55 +0530

    added basics of functions

---
+++ Starting_Steps_Basics/functions/ignoring_returns.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	firstName, lastName := getNames()
+	
+	// Welcome message
+	fmt.Println("Welcome to TEXTIO ", firstName)
+}
+
+func getNames()(string, string){
+	
+}
