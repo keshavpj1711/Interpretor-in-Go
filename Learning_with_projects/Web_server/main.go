ac29a4a228e110c259c6db4e257b0741aa6b6063
keshavpj1711 <keshavpj1711@gmail.com>
2024-01-24 01:43:45 +0530

    completed main() for web_server functions yet to be created

--- Learning_with_projects/Web_server/main.go
+++ Learning_with_projects/Web_server/main.go
@@ -7,5 +7,18 @@ import(
 )
 
 func main(){
+	fileServer := http.FileServer(http.Dir("./static"))  // We want to checkout static folder
+	// GO, PHP all these lang by default look for index.html
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 	
+	fmt.Println("Starting server at port 8080")
+
+	// Error Handling
+	// Checking if the server starts or not
+	if err := http.ListenAndServe("8080", nil); err != nil{
+		log.Fatal(err)
+	}
+
 }
