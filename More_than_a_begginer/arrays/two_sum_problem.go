b066c3e16f6587018128a42863a09377cd445f07
keshavpj1711 <keshavpj1711@gmail.com>
2024-01-28 21:27:40 +0530

    added function for main computation twoSum.go

--- More_than_a_begginer/arrays/two_sum_problem.go
+++ More_than_a_begginer/arrays/two_sum_problem.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// Creating a function to do our requiored task
+func twoSum(nums []int, target int) []int {
+	var output []int
+
+	for i := 0; i < len(nums); i++{
+		for j := i; j < len(nums); j++{
+			if nums[i] + nums[j] == target{
+				output = append(output, i, j)
+			}
+		}
+	}
+
+	return output
+}
+
 func main(){
 
 	// Declaring and taking array as input
