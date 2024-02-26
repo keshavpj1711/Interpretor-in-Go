011d820a63c17ab1a3a280ff2d6bd56f7f1b1d99
keshavpj1711 <keshavpj1711@gmail.com>
2024-02-27 01:50:34 +0530

    issue resolved regarding available port

--- Learning_with_projects/Web_server/main.go
+++ Learning_with_projects/Web_server/main.go
@@ -1,19 +1,19 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
 
-//For any API call we have a response and request and this is similar to what we have
+// For any API call we have a response and request and this is similar to what we have
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello"{
-		http.Error(w, "404 Not Found", http.StatusNotFound)
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Found", http.StatusNotFound)
 		return
 	}
 
-	if r.Method != "GET"{
+	if r.Method != "GET" {
 		http.Error(w, "Method is not supported", http.StatusNotFound)
 		return
 	}
@@ -25,31 +25,29 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	
-	fmt.Fprintf(w, "Post Request Successful")
-	
+
+	fmt.Fprintf(w, "Post Request Successful\n")
+
 	name := r.FormValue("name")
 	address := r.FormValue("address")
-	
+
 	fmt.Fprintf(w, "Name = %s\n", name)
 	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
-func main(){
-	fileServer := http.FileServer(http.Dir("./static"))  // We want to checkout static folder
+func main() {
+	fileServer := http.FileServer(http.Dir("./static")) // We want to checkout static folder
 	// GO, PHP all these lang by default look for index.html
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
-	
+
 	fmt.Println("Starting server at port 8080")
 
 	// Error Handling
 	// Checking if the server starts or not
-	if err := http.ListenAndServe("8080", nil); err != nil{
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 
-	// The problem i am currently facing is everytime i check for open ports, 
-	// once i try to connect to it shows this invalid port or service name in host:port pair (SA1020)
 }
